10f3d8665acd0ce1866856037fbffcdf6651749a
Solo Chevez <davidchevez90@gmail.com>
2023-03-24 21:00:57 -0600

    moved auth showcase to navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { Avatar } from 'antd'
-import { useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -14,16 +14,39 @@ export function Layout({ children } : { children: JSX.Element }) {
 }
 
 function Navbar() {
-  const { data: sessionData } = useSession()
-
   return(
     <div className='flex items-center w-full justify-between gap-12 px-8 pt-6 pb-3'>
       <Link href='/' className='text-white'>
         <Image alt='Home' src='/hitplaylogo.png' width={45} height={45} />
       </Link>
-      <Link href='/profile' className='text-white'>
-        <Avatar src={sessionData?.user.image}/>
-      </Link>
+      <AuthShowcase />
     </div>
   )
+}
+
+const AuthShowcase = () => {
+  const { data: sessionData } = useSession();
+  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+  //   undefined, // no input
+  //   { enabled: sessionData?.user !== undefined },
+  // );
+  return (
+    <div className="flex items-center justify-center gap-4">
+      {/* <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p> */}
+      <button
+        className="rounded-full bg-white/10 px-10 py-2 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+      { sessionData && 
+        <Link href='/profile' className='text-white'>
+          <Avatar size={39} src={sessionData?.user.image}/>
+        </Link>
+      }
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Head from "next/head";
 import { type NextPage } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { api } from '~/utils/api';
 import Image from 'next/image';
 // import { type PlaylistTrack, type Track } from '~/utils/types';
@@ -107,7 +107,6 @@ const Home: NextPage = () => {
             <TrackStandings tracks={tracksInDb} />
           </div>
         }
-        <AuthShowcase />
       </main>
     </>
   );
@@ -171,29 +170,7 @@ const TrackCard = ({ track } : { track: TrackFromDb }) => {
         </> :
         <h1 className='text-white h-full'>Sign in to hear a preview!</h1>
       }
-      
     </div>
   )
 }
 
-const AuthShowcase = () => {
-  const { data: sessionData } = useSession();
-  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined },
-  // );
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      {/* <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p> */}
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-}
