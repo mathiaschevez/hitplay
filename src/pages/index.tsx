65bc17e5eb5bacf21347816a7673f31c95a043ed
Solo Chevez <davidchevez90@gmail.com>
2023-03-25 17:13:13 -0600

    added spinner if no track cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { type NextPage } from 'next';
 import { useSession } from 'next-auth/react';
 import { api } from '~/utils/api';
 import Image from 'next/image';
+import { Spin } from 'antd';
 // import { type PlaylistTrack, type Track } from '~/utils/types';
 
 // interface PlaylistTracksData {
@@ -90,23 +91,26 @@ const Home: NextPage = () => {
         <meta name='description' content='Find the best music for your playlists' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className='flex flex-col items-center justify-center gap-12 px-4 py-1 w-full'>
+      <main className='flex flex-col items-center justify-center px-4 py-1 w-full flex-1'>
         <h1 className='text-5xl text-[hsl(280,100%,70%)] font-extrabold tracking-tight sm:text-[5rem]'>Hitplay</h1>
-        { tracksInDb &&
-          <div className='flex gap-3 justify-around w-full'>
-            <div className='flex flex-wrap gap-10 items-center justify-center'>
-              {currentTracks?.[0] && currentTracks?.[1] && currentTracks?.map((track, i) => (
-                <div key={track?.id} className='flex flex-col gap-6'>
-                  { track && <TrackCard track={track} />}
-                  <button onClick={() => void handleVote(i)} className='text-white font-bold border-2 rounded-full py-2 hover:bg-purple-600'>
-                    Vote
-                  </button>
-                </div>
-              ))}
-            </div>
-            <TrackStandings tracks={tracksInDb} />
-          </div>
-        }
+        <div className='flex gap-3 justify-around w-full flex-1 items-center'>
+          { tracksInDb && currentTracks ?
+            <>
+              <div className='flex flex-wrap gap-10 items-center justify-center'>
+                {currentTracks?.[0] && currentTracks?.[1] && currentTracks?.map((track, i) => (
+                  <div key={track?.id} className='flex flex-col gap-6'>
+                    { track && <TrackCard track={track} />}
+                    <button onClick={() => void handleVote(i)} className='text-white font-bold border-2 rounded-full py-2 hover:bg-purple-600'>
+                      Vote
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <TrackStandings tracks={tracksInDb} />
+            </> : 
+            <Spin />
+          }
+        </div>
       </main>
     </>
   );
